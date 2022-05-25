94f24db65a156636e39c880c468809d487d5f4f2
CATHA2 <DJH20002@gmail.com>
2022-05-25 17:45:57 +0300

    Action

--- Source/Behavioral/Action.py
+++ Source/Behavioral/Action.py
@@ -21,8 +21,7 @@ class SimpleCommand(Command):
         self._payload = payload
 
     def execute(self) -> None:
-        print(f"SimpleCommand: See, I can do simple things like printing"
-              f"({self._payload})")
+        print(f"Простоая команада (печать): ({self._payload})")
 
 
 class ComplexCommand(Command):
@@ -30,25 +29,22 @@ class ComplexCommand(Command):
     Но есть и команды, которые делегируют более сложные операции другим
     объектам, называемым «получателями».
     """
-
-    def __init__(self, receiver: Receiver, a: str, b: str) -> None:
+    def __init__(self, receive: Receiver, *commands: str) -> None:
         """
         Сложные команды могут принимать один или несколько объектов-получателей
         вместе с любыми данными о контексте через конструктор.
         """
-
-        self._receiver = receiver
-        self._a = a
-        self._b = b
+        self._receiver = receive
+        self._commands = commands
 
     def execute(self) -> None:
         """
         Команды могут делегировать выполнение любым методам получателя.
         """
-
-        print("ComplexCommand: Complex stuff should be done by a receiver object", end="")
-        self._receiver.do_something(self._a)
-        self._receiver.do_something_else(self._b)
+        print("Сложная команда: с помощью получателя объекта")
+        self._receiver.do_something(self._commands[0])
+        for command in self._commands[1:]:
+            self._receiver.do_something_else(command)
 
 
 class Receiver:
@@ -58,11 +54,13 @@ class Receiver:
     может выступать Получателем.
     """
 
-    def do_something(self, a: str) -> None:
-        print(f"\nReceiver: Working on ({a}.)", end="")
+    @staticmethod
+    def do_something(a: str) -> None:
+        print(f"PC: Работая над ({a}.)")
 
-    def do_something_else(self, b: str) -> None:
-        print(f"\nReceiver: Also working on ({b}.)", end="")
+    @staticmethod
+    def do_something_else(b: str) -> None:
+        print(f"PC: также работает над ({b}.)")
 
 
 class Invoker:
@@ -70,13 +68,15 @@ class Invoker:
     Отправитель связан с одной или несколькими командами. Он отправляет запрос
     команде.
     """
-
+    _on_commands = None
     _on_start = None
     _on_finish = None
 
     """
     Инициализация команд.
     """
+    def set_commands(self, commands: Command):
+        self._on_commands = commands
 
     def set_on_start(self, command: Command):
         self._on_start = command
@@ -84,32 +84,25 @@ class Invoker:
     def set_on_finish(self, command: Command):
         self._on_finish = command
 
-    def do_something_important(self) -> None:
+    def action(self) -> None:
         """
         Отправитель не зависит от классов конкретных команд и получателей.
         Отправитель передаёт запрос получателю косвенно, выполняя команду.
         """
-
-        print("Invoker: Does anybody want something done before I begin?")
+        print("Начало простого:")
         if isinstance(self._on_start, Command):
             self._on_start.execute()
-
-        print("Invoker: ...doing something really important...")
-
-        print("Invoker: Does anybody want something done after I finish?")
+        print(" --- выполнение ---\nКонец простого:")
+        print("Начало сложного:")
         if isinstance(self._on_finish, Command):
             self._on_finish.execute()
+        print(" --- выполнение ---\nКонец сложного:")
 
 
 if __name__ == "__main__":
-    """
-    Клиентский код может параметризовать отправителя любыми командами.
-    """
-
     invoker = Invoker()
-    invoker.set_on_start(SimpleCommand("Say Hi!"))
+    invoker.set_on_start(SimpleCommand("Hello!"))
     receiver = Receiver()
     invoker.set_on_finish(ComplexCommand(
-        receiver, "Send email", "Save report"))
-
-    invoker.do_something_important()
+        receiver, "Delete files", "Open dir", "del pass", "block pc"))
+    invoker.action()
